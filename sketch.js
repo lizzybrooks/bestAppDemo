f65475b9445bc7498b7c5da4f3d66cf4ad1ee62b
Lizzy <lizzy.brooks@gmail.com>
2022-02-28 13:51:16 -0800

    added files

---
+++ sketch.js
@@ -0,0 +1,37 @@
+
+let button;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(220);
+  
+  
+  button = createButton('click me');
+  button.position(width/2-20, height/6);
+  button.center();
+  button.style('background-color', "pink");
+  button.style('border','none');
+  button.style('padding','15px');
+  button.style('font-size','26px');
+  button.style('border-radius', '12px')
+  
+
+  
+  button.mousePressed(changeBG);
+}
+
+function draw() {
+  
+  fill("teal");
+  noStroke();
+  textFont("Helvetica");
+  textAlign(CENTER);
+  textSize(43)
+  text("BEST APP",width/2,height/4)
+  
+}
+
+function changeBG() {
+  let val = random(255);
+  background(val);
+}
